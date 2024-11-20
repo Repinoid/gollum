6d8bf402a512e9294d612b04a33e52629beb2302
Tatare <tazet@narod.ru>
2024-11-20 14:07:17 +0300

    pand

--- goim.go
+++ goim.go
@@ -4,11 +4,10 @@ import (
 	"math/rand/v2"
 )
 
-
 func CreateArray(arrayLength, maxint int) (arra []int) {
 	arra = make([]int, arrayLength)
 	for i := range arra {
-		arra[i] = 3 //rand.IntN(maxint)
+		arra[i] = rand.IntN(maxint)
 	}
 	return
 }
